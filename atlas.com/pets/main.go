d267fe7f3bc4194435672221e5e16cf44f7fac07
Chronicle20 <a.chronicle.20@gmail.com>
2025-03-09 12:04:38 -0400

    Initial commit.

---
+++ atlas.com/pets/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"atlas-pets/logger"
+	"atlas-pets/service"
+	"atlas-pets/tracing"
+	"github.com/Chronicle20/atlas-rest/server"
+)
+
+const serviceName = "atlas-pets"
+
+type Server struct {
+	baseUrl string
+	prefix  string
+}
+
+func (s Server) GetBaseURL() string {
+	return s.baseUrl
+}
+
+func (s Server) GetPrefix() string {
+	return s.prefix
+}
+
+func GetServer() Server {
+	return Server{
+		baseUrl: "",
+		prefix:  "/api/",
+	}
+}
+
+func main() {
+	l := logger.CreateLogger(serviceName)
+	l.Infoln("Starting main service.")
+
+	tdm := service.GetTeardownManager()
+
+	tc, err := tracing.InitTracer(l)(serviceName)
+	if err != nil {
+		l.WithError(err).Fatal("Unable to initialize tracer.")
+	}
+
+	server.CreateService(l, tdm.Context(), tdm.WaitGroup(), GetServer().GetPrefix())
+
+	tdm.TeardownFunc(tracing.Teardown(l)(tc))
+
+	tdm.Wait()
+	l.Infoln("Service shutdown.")
+}
